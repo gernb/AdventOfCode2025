d2a309c11267e888f32194dfc5c49ddc7354f29f
peter <peter@1dot0.net>
2025-12-01 21:41:28 -0800

    Day 2, part 2

--- Sources/Day 02/Solution.swift
+++ Sources/Day 02/Solution.swift
@@ -42,6 +42,40 @@ enum Part1 {
 
 enum Part2 {
   static func run(_ source: InputData) {
-    print("Part 2 (\(source)): TODO")
+    var invalidIds: [Int] = []
+
+    // brute force
+    for range in Part1.parse(source.data) {
+      for idNum in range {
+        let id = String(idNum)
+        let halfLen = id.count / 2
+        guard Set(Array(id)).count <= halfLen else { continue }
+        var subIdLen = 1
+        while subIdLen <= halfLen {
+          defer { subIdLen += 1 }
+          guard id.count.isMultiple(of: subIdLen) else { continue }
+          let subIds = id.split(len: subIdLen)
+          if subIds.allSatisfy({ $0 == subIds[0] }) {
+            invalidIds.append(idNum)
+            break
+          }
+        }
+      }
+    }
+
+    let result = invalidIds.reduce(0, +)
+    print("Part 2 (\(source)): \(result)")
+  }
+}
+
+extension String {
+  func split(len: Int) -> [Substring] {
+    var result: [Substring] = []
+    var start = 0
+    while start < count {
+      result.append(self.dropFirst(start).prefix(len))
+      start += len
+    }
+    return result
   }
 }

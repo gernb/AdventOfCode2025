a9b1fdc5badb5123ab4d1dc36df886196cc31a16
peter <peter@1dot0.net>
2025-12-01 21:16:24 -0800

    Day 2, part 1

--- Sources/Day 02/Solution.swift
+++ Sources/Day 02/Solution.swift
@@ -10,15 +10,38 @@ import Foundation
 // MARK: - Part 1
 
 enum Part1 {
-    static func run(_ source: InputData) {
-        print("Part 1 (\(source)): TODO")
+  static func parse(_ line: String) -> [ClosedRange<Int>] {
+    line.components(separatedBy: ",")
+      .map {
+        let numbers = $0.components(separatedBy: "-").compactMap(Int.init)
+        return numbers[0] ... numbers[1]
+      }
+  }
+
+  static func run(_ source: InputData) {
+    var invalidIds: [Int] = []
+
+    // brute force
+    for range in parse(source.data) {
+      for idNum in range {
+        let id = String(idNum)
+        guard id.count.isMultiple(of: 2) else { continue }
+        let halfLen = id.count / 2
+        if id.prefix(halfLen) == id.suffix(halfLen) {
+          invalidIds.append(idNum)
+        }
+      }
     }
+
+    let result = invalidIds.reduce(0, +)
+    print("Part 1 (\(source)): \(result)")
+  }
 }
 
 // MARK: - Part 2
 
 enum Part2 {
-    static func run(_ source: InputData) {
-        print("Part 2 (\(source)): TODO")
-    }
+  static func run(_ source: InputData) {
+    print("Part 2 (\(source)): TODO")
+  }
 }

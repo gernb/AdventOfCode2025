bdbc857fee4be246125b18dd18da5ba9de6e3e9e
peter bohac <gern@1dot0.net>
2025-12-04 21:31:38 -0800

    Day 5, part 1

--- Sources/Day 05/InputData.swift
+++ Sources/Day 05/InputData.swift
@@ -26,5 +26,16 @@ struct InputData: CustomStringConvertible {
         name: "example",
         data:
 """
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
 """)
 }
--- Sources/Day 05/Solution.swift
+++ Sources/Day 05/Solution.swift
@@ -10,15 +10,34 @@ import Foundation
 // MARK: - Part 1
 
 enum Part1 {
-    static func run(_ source: InputData) {
-        print("Part 1 (\(source)): TODO")
+  static func parse(_ lines: [String]) -> (ranges: [ClosedRange<Int>], availableIds: [Int]) {
+    let parts = lines.split(separator: "")
+    let ranges: [ClosedRange<Int>] = parts[0].reduce(into: []) { result, line in
+      let numbers = line.components(separatedBy: "-").compactMap(Int.init)
+      result.append(numbers[0] ... numbers[1])
     }
+    let availableIds = parts[1].compactMap(Int.init)
+    return (ranges, availableIds)
+  }
+
+  static func run(_ source: InputData) {
+    let (ranges, availableIds) = parse(source.lines)
+    let available = availableIds.count { id in
+      for range in ranges {
+        if range.contains(id) {
+          return true
+        }
+      }
+      return false
+    }
+    print("Part 1 (\(source)): \(available)")
+  }
 }
 
 // MARK: - Part 2
 
 enum Part2 {
-    static func run(_ source: InputData) {
-        print("Part 2 (\(source)): TODO")
-    }
+  static func run(_ source: InputData) {
+    print("Part 2 (\(source)): TODO")
+  }
 }

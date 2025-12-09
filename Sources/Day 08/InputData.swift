91d9c9b69b39a66e9bf85af1c26b99c7f76648ae
peter <peter@1dot0.net>
2025-12-08 20:57:14 -0800

    Day 8, part 1

--- Sources/Day 08/InputData.swift
+++ Sources/Day 08/InputData.swift
@@ -9,14 +9,15 @@ struct InputData: CustomStringConvertible {
     static let day = 8
     static func part1(_ challengeData: String?) -> [Self?] {[
         .example,
-        challengeData.map { Self(name: "challenge", data: $0) }
+        challengeData.map { Self(name: "challenge", joinCount: 1000, data: $0) }
     ]}
     static func part2(_ challengeData: String?) -> [Self?] {[
         .example,
-        challengeData.map { Self(name: "challenge", data: $0) }
+        challengeData.map { Self(name: "challenge", joinCount: 1000, data: $0) }
     ]}
 
     let name: String
+    let joinCount: Int
     let data: String
 
     var lines: [String] { data.components(separatedBy: .newlines) }
@@ -24,7 +25,28 @@ struct InputData: CustomStringConvertible {
 
     static let example = Self(
         name: "example",
+        joinCount: 10,
         data:
 """
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
 """)
 }
--- Sources/Day 08/Solution.swift
+++ Sources/Day 08/Solution.swift
@@ -9,16 +9,76 @@ import Foundation
 
 // MARK: - Part 1
 
+struct Coordinate: Hashable, CustomStringConvertible {
+  var x: Int
+  var y: Int
+  var z: Int
+
+  static let zero: Self = .init(x: 0, y: 0, z: 0)
+
+  var description: String { "(\(x), \(y), \(z))" }
+
+  func distance(to other: Self) -> Double {
+    sqrt(pow(Double(x - other.x), 2) + pow(Double(y - other.y), 2) + pow(Double(z - other.z), 2))
+  }
+}
+extension Coordinate {
+  // 162,817,812
+  init(string: any StringProtocol) {
+    let numbers = string.components(separatedBy: ",").compactMap(Int.init)
+    self.init(x: numbers[0], y: numbers[1], z: numbers[2])
+  }
+}
+
+struct Pair<T>: Hashable where T: Hashable {
+  let a: T
+  let b: T
+  var set: Set<T> {
+    [a, b]
+  }
+}
+
 enum Part1 {
-    static func run(_ source: InputData) {
-        print("Part 1 (\(source)): TODO")
+  static func run(_ source: InputData) {
+    let junctions = source.lines.map(Coordinate.init(string:))
+
+    let distances: [Pair<Coordinate>: Double] = junctions.indices.reduce(into: [:]) { result, index in
+      let a = junctions[index]
+      for b in junctions.dropFirst(index + 1) {
+        result[Pair(a: a, b: b)] = a.distance(to: b)
+      }
     }
+
+    let shortest = distances.sorted { $0.value < $1.value }
+    var circuits: [Set<Coordinate>] = []
+    for count in (0 ..< source.joinCount) {
+      let pair = shortest[count].key
+      let indexOfA = circuits.firstIndex { $0.contains(pair.a) }
+      let indexOfB = circuits.firstIndex { $0.contains(pair.b) }
+      switch (indexOfA, indexOfB) {
+      case (.none, .none):
+        circuits.append(pair.set)
+      case (.none, .some(let index)), (.some(let index), .none):
+        circuits[index].formUnion(pair.set)
+      case (.some(let a), .some(let b)):
+        guard a != b else { continue }
+        circuits[a].formUnion(circuits[b])
+        circuits.remove(at: b)
+      }
+    }
+
+    let result = circuits.map { $0.count }
+      .sorted { $0 > $1 }
+      .prefix(3)
+      .reduce(1, *)
+    print("Part 1 (\(source)): \(result)")
+  }
 }
 
 // MARK: - Part 2
 
 enum Part2 {
-    static func run(_ source: InputData) {
-        print("Part 2 (\(source)): TODO")
-    }
+  static func run(_ source: InputData) {
+    print("Part 2 (\(source)): TODO")
+  }
 }

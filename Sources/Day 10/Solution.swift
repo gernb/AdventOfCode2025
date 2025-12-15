c56ec1b33aa982562bd7bb79d306898e4435f908
peter <peter@1dot0.net>
2025-12-14 19:18:40 -0800

    Day 10, part 2

--- Sources/Day 10/Solution.swift
+++ Sources/Day 10/Solution.swift
@@ -101,9 +101,93 @@ enum Part1 {
 }
 
 // MARK: - Part 2
+// Implementation of the design found here: https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
 
+extension Array where Element == Int {
+  func pressingButton(_ button: [Int]) -> Self {
+    var joltages = self
+    for idx in button {
+      joltages[idx] -= 1
+    }
+    return joltages
+  }
+}
+
+extension Int {
+  var isEven: Bool { isMultiple(of: 2) }
+  var isOdd: Bool { !isEven }
+
+  func bits(width: Int) -> [Bool] {
+    (0 ..< width)
+      .map { (1 << $0) & self > 0 }
+      .reversed()
+  }
+}
+
+@MainActor
 enum Part2 {
+  static var buttons: [[Int]] = []
+
+  private static var lightsMemo: [[Bool]: [[Int]]] = [:]
+  static func allButtons(for lights: [Bool]) -> [[Int]] {
+    if let cached = lightsMemo[lights] {
+      return cached
+    }
+
+    let combinations: [[Int]] = (0 ..< (1 << buttons.count)).reduce(into: []) { result, combo in
+      let press = combo.bits(width: buttons.count)
+      let idxs = press.enumerated()
+        .filter { $0.element }
+        .map(\.offset)
+      let finalLights = idxs.reduce(0.bits(width: lights.count)) { partialResult, idx in
+        partialResult.pressingButton(buttons[idx])
+      }
+      if finalLights == lights {
+        result.append(idxs)
+      }
+    }
+    lightsMemo[lights] = combinations
+    return combinations
+  }
+
+  private static var joltagesMemo: [[Int]: Int] = [:]
+  static func buttonPresses(for joltages: [Int]) -> Int? {
+    guard joltages.allSatisfy({ $0 >= 0 }) else {
+      return nil
+    }
+    // stop if all zeros
+    if joltages.reduce(0, +) == 0 {
+      return 0
+    }
+
+    if let cached = joltagesMemo[joltages] {
+      return cached == .max ? nil : cached
+    }
+
+    let lights = joltages.map(\.isOdd)
+    let combos = allButtons(for: lights)
+    let counts = combos.map { combo in
+      let nextJoltages = combo.reduce(joltages) { partialResult, idx in
+        partialResult.pressingButton(buttons[idx])
+      }
+      .map { $0 / 2 }
+      return buttonPresses(for: nextJoltages).map { 2 * $0 + combo.count } ?? .max
+    }
+    let count = counts.min() ?? .max
+
+    joltagesMemo[joltages] = count
+    return count == .max ? nil : count
+  }
+
   static func run(_ source: InputData) {
-    print("Part 2 (\(source)): TODO")
+    let machines = source.lines.map(Machine.init)
+    let presses = machines.map { machine in
+      buttons = machine.buttons
+      lightsMemo.removeAll()
+      joltagesMemo.removeAll()
+      return buttonPresses(for: machine.joltages)!
+    }
+    let result = presses.reduce(0, +)
+    print("Part 2 (\(source)): \(result)")
   }
 }

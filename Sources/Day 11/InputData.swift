ae90c9b40d37b82d6f057b5e36135b776dd11ce5
peter <peter@1dot0.net>
2025-12-14 20:23:04 -0800

    Day 11, part 1

--- Sources/Day 11/InputData.swift
+++ Sources/Day 11/InputData.swift
@@ -26,5 +26,15 @@ struct InputData: CustomStringConvertible {
         name: "example",
         data:
 """
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
 """)
 }
--- Sources/Day 11/Solution.swift
+++ Sources/Day 11/Solution.swift
@@ -7,18 +7,60 @@
 
 import Foundation
 
+final class Node {
+  let id: String
+  let connections: [String]
+
+  init(id: String, connections: [String]) {
+    self.id = id
+    self.connections = connections
+  }
+}
+extension Node {
+  static func parse(_ lines: [String]) -> [String: Node] {
+    lines.reduce(into: [:]) { result, line in
+      let node = Node(line)
+      result[node.id] = node
+    }
+  }
+
+  // you: bbb ccc
+  convenience init(_ line: String) {
+    let parts = line.components(separatedBy: ": ")
+    self.init(id: parts[0], connections: parts[1].components(separatedBy: " "))
+  }
+}
+
 // MARK: - Part 1
 
+@MainActor
 enum Part1 {
-    static func run(_ source: InputData) {
-        print("Part 1 (\(source)): TODO")
+  static var nodes: [String: Node] = [:]
+
+  static func allPaths(from path: [String], to end: String) -> [[String]] {
+    let last = path.last!
+    return nodes[last]!.connections.flatMap { next in
+      let path = path + [next]
+      if next == end {
+        return [path]
+      } else {
+        return allPaths(from: path, to: end)
+      }
     }
+  }
+
+  static func run(_ source: InputData) {
+    nodes = Node.parse(source.lines)
+    let paths = allPaths(from: ["you"], to: "out")
+    let result = paths.count
+    print("Part 1 (\(source)): \(result)")
+  }
 }
 
 // MARK: - Part 2
 
 enum Part2 {
-    static func run(_ source: InputData) {
-        print("Part 2 (\(source)): TODO")
-    }
+  static func run(_ source: InputData) {
+    print("Part 2 (\(source)): TODO")
+  }
 }
